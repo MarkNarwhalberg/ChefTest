01567ccc31877349e702165d768211bb2c71e6fa
Josh Dober <MarkNarwhalberg@users.noreply.github.com>
2017-05-19 19:06:23 -0700

    Added lab-visage cookbook with Telnet install

---
+++ cookbooks/lab-visage/recipes/WindowsFeatures.rb
@@ -0,0 +1,5 @@
+#installs Windows Features
+
+windows_feature "Telnet-Client" do
+  action :install
+end
---
+++ roles/lab-visage.rb
@@ -0,0 +1,3 @@
+name "lab-visage"
+description "Testing Windows feature installs"
+run_list "recipe[lab-visage::WindowsFeatures]"
